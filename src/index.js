67cd6f3847f6b7b0780b5f7f206e5f99446c96bd
haichannn <muhammadhaichal304@gmail.com>
2025-08-13 21:37:49 +0700

    feat: create root URL

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// This is a simple route that returns a welcome message
+// You can access it by navigating to http://localhost:3000/
+app.get("/", (req, res) => {
+  res.send("Welcome to the Express.js server!");
+})
+
+app.listen(PORT, (error) => {
+ 
+  if (error) {
+    console.error("Error starting the server:", error);
+  }
+
+  console.log(`Server is running on http://localhost:${PORT}`);
+})
